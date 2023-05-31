5b7529242ee32ba8853b306e6c2374a9989afe52
Imrul Anwar <imrulanwar836@gmail.com>
2023-05-31 10:12:07 +0600

    Added swipe to dismiss functionality

--- app/src/main/java/com/example/dailytasks/ui/todo_list/TodoItem.kt
+++ app/src/main/java/com/example/dailytasks/ui/todo_list/TodoItem.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -25,8 +27,9 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.dailytasks.data.Todo
-import com.example.dailytasks.ui.todo_list.TodoListEvent
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TodoItem(
     todo: Todo,
--- app/src/main/java/com/example/dailytasks/ui/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/example/dailytasks/ui/todo_list/TodoListScreen.kt
@@ -1,13 +1,16 @@
 package com.example.dailytasks.ui.todo_list
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,6 +19,8 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -25,18 +30,26 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 
 import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,6 +58,7 @@ import com.example.dailytasks.ui.todo_list.TodoListViewModel
 import com.example.dailytasks.util.UiEvent
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.example.dailytasks.ui.theme.myThemeColor
+import kotlin.math.roundToInt
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -79,7 +93,9 @@ fun TodoListScreen(
             title = {
                 Text(
                     text = "All Tasks    ",
-                    modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally),
                     fontSize = 40.sp
                 )
             },
@@ -112,18 +128,45 @@ fun TodoListScreen(
                 modifier = Modifier.fillMaxSize()
             ) {
                 items(todos.value) { todo ->
-                    TodoItem(
-                        todo = todo,
-                        onEvent = viewModel::onEvent,
+                    var offsetX by remember { mutableStateOf(0f) }
+                    val dismissThreshold = 100.dp
+
+                    Box(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .clickable {
-                                viewModel.onEvent(TodoListEvent.OnTodoClick(todo))
+                            .pointerInput(Unit) {
+                                detectDragGestures { change, dragAmount ->
+                                    offsetX += dragAmount.x
+                                    when {
+                                        offsetX > dismissThreshold.toPx() -> {
+                                            // Swiped to dismiss, handle the dismiss event here
+                                            viewModel.onEvent(TodoListEvent.OnDeleteTodoClick(todo))
+                                        }
+                                        offsetX < -dismissThreshold.toPx() -> {
+                                            // Swiped in the opposite direction, reset the offset
+                                            offsetX = 0f
+                                        }
+                                    }
+                                    change.consumePositionChange()
+                                }
                             }
-                            .padding(16.dp)
-                    )
+                            .offset { IntOffset(offsetX.roundToInt(), 0) }
+                    ) {
+                        TodoItem(
+                            todo = todo,
+                            onEvent = viewModel::onEvent,
+                            modifier = Modifier
+                                .clickable {
+                                    viewModel.onEvent(TodoListEvent.OnTodoClick(todo))
+                                }
+                                .padding(16.dp)
+                        )
+                    }
                 }
             }
+
+
+
         }
     }
 }
